f4a53eae7823c34066cab4e8016a7f7226bc0d9c
Maciej Kurek <maciejk77@gmail.com>
2024-04-11 15:13:13 +0100

    Add UI components, /currencies endpoint

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,5 +1,71 @@
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div>App</div>;
+  const [currencies, setCurrencies] = useState([]);
+  const [amount, setAmount] = useState(null);
+  const [baseCurrency, setBaseCurrency] = useState();
+  const [targetCurrency, setTargetCurrency] = useState("");
+
+  console.log("from: ", baseCurrency);
+  console.log("to: ", targetCurrency);
+  console.log("amount: ", amount);
+
+  useEffect(() => {
+    fetch("/currencies")
+      .then((res) => res.json())
+      .then((data) => setCurrencies(data));
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    setAmount(value);
+  };
+
+  const handleBaseCurrencyChange = (e) => {
+    const { value } = e.target;
+    setBaseCurrency(value);
+  };
+
+  const handleTargetCurrencyChange = (e) => {
+    const { value } = e.target;
+    setTargetCurrency(value);
+  };
+
+  return (
+    <div style={styles.flex}>
+      <div style={styles.flex}>
+        <div>Base:</div>
+        <input onChange={handleInputChange} type="number" />
+        <select onChange={handleBaseCurrencyChange}>
+          {currencies.map((currency) => (
+            <option key={currency} value={currency} name={currency}>
+              {currency}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div style={styles.flex}>
+        <div>Exchange to:</div>
+        <select onChange={handleTargetCurrencyChange}>
+          {currencies.map((currency) => {
+            return (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+      <button type="button">Exchange here</button>
+    </div>
+  );
 }
+
+const styles = {
+  flex: {
+    display: "flex",
+  },
+};
 
 export default App;
--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,15 @@
-import express from "express";
-import ViteExpress from "vite-express";
+import express from 'express';
+import ViteExpress from 'vite-express';
 
 const app = express();
+const BASE_URL = 'https://open.er-api.com/v6/latest';
 
-app.get("/hello", (req, res) => {
-  res.send({foo: 1});
+app.get('/currencies', async (req, res) => {
+  const { rates } = await fetch(BASE_URL).then((res) => res.json());
+
+  https: res.send(Object.keys(rates));
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+  console.log('Server is listening on port 3000...')
 );
