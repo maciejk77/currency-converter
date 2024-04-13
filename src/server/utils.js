70edfe2f4424d1d00e638fddbf2333c4efee3a6e
Maciej Kurek <maciejk77@gmail.com>
2024-04-13 18:09:30 +0100

    Clean up, writeToHistory util

--- src/server/utils.js
+++ src/server/utils.js
@@ -2,17 +2,18 @@ import fs from 'fs';
 import { filePath } from './config.js';
 
 export const writeToHistory = (historyItem) => {
-  let history = [];
-
   if (fs.existsSync(filePath)) {
     try {
-      history = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+      const history = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
       fs.writeFileSync(filePath, JSON.stringify([historyItem, ...history]));
     } catch (err) {
       console.log(err);
     }
   } else {
-    history.push(historyItem);
-    fs.writeFileSync(filePath, JSON.stringify(history));
+    try {
+      fs.writeFileSync(filePath, JSON.stringify([historyItem]));
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
